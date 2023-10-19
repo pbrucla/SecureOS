4bb3d902c444df0989591c2fe9f6f55dd86bec7b
Daniel Yang <danielyangkang@gmail.com>
2023-10-18 19:44:10 -0700

    commented out loopy code

--- source/kernel/kernel.c
+++ source/kernel/kernel.c
@@ -12,9 +12,9 @@ void main()
     init_drivers();
     terminal_clear();
     unsigned long long cur_time = get_cpu_time();
-    // terminal_putchar('X');
-    // terminal_put64(cur_time);
-    // terminal_update_cursor();
+    terminal_putchar('X');
+    terminal_put64(cur_time);
+    terminal_update_cursor();
     // string s;
     // s.data = "HEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEELLLLLLLLLLLLLLLLLLLLLLLLLLL"
     //          "LLOOOOOOOOOOOOOOOOOOOOOOOOOOOOO";
@@ -24,12 +24,12 @@ void main()
     // palloc(wow, PAGE_PRESENT | PAGE_RW);
     // *wow = 0x42042069;
     // terminal_put64(*wow);
-    for (int i = 0; i < 30; i ++) {
-        uint32_t* smol = kalloc(200);
-        *smol = 0x12345678;
-        terminal_put64(smol);
-        terminal_putchar(' ');
-        terminal_put64(*smol);
-        terminal_putchar('\n');
-    }
+    // for (int i = 0; i < 30; i ++) {
+    //     uint32_t* smol = kalloc(200);
+    //     *smol = 0x12345678;
+    //     terminal_put64(smol);
+    //     terminal_putchar(' ');
+    //     terminal_put64(*smol);
+    //     terminal_putchar('\n');
+    // }
 }
